7c0f04d4d7b7e4cf9738d4fe8ff84e969750c526
Agustín Cabero <agustincabero@gmail.com>
2019-06-25 19:15:06 -0300

    Challenge complete. :)

--- js/index.js
+++ js/index.js
@@ -8,38 +8,39 @@ const urlFirstStep = "https://us-central1-verto-backend-dev.cloudfunctions.net/c
 (function(){
 
   //Module data
-  let urlLastStep, idLastStep;
+  let urlLastStep = "https://us-central1-verto-backend-dev.cloudfunctions.net/challengeFinalStep";
+  let idLastStep = "vl69qnI4Qoo5gK43RSLq";
 
   //DOM
-  const btnFirstStep = document.querySelector("");
-  const btnLastStep = document.querySelector("");
-  const txtEmail = document.querySelector("input#emilio");
+  const btnFirstStep = document.querySelector("button#first");
+  const btnLastStep = document.querySelector("button.last");
+  const txtEmail = document.querySelector("input#email");
   const txtName = document.querySelector("input#name");
   const txtMsg = document.querySelector("span#msg");
 
   //Events
   btnFirstStep.addEventListener("click", gotoFirstStep);
-  btnLastStep.addEventListeners("click", );
+  btnLastStep.addEventListener("click", gotoLastStep);
 
   //Callbacks
   function gotoFirstStep(){
 
     //First Step. Using promises
     fetch(urlFirstStep, {
+      method: "POST",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-      },
-      method: "POST",
+      },      
       body: JSON.stringify({
-        token: window.appToken,
-        email: txtEmail.value
+        'token': window.appToken,
+        'email': txtEmail.value
       })
     })
     .then(response=>response.json())
     .then(data=>{
-      console.log("FromServer", data);
       btnLastStep.disabled = false;
+      console.log("FromServer", data);
     });
 
   }
@@ -54,8 +55,9 @@ const urlFirstStep = "https://us-central1-verto-backend-dev.cloudfunctions.net/c
       },
       method: "POST",
       body: JSON.stringify({
-        name: txtName.value,
-        id: idLastStep
+        'token': window.appToken,
+        'name': txtName.value,
+        'id': idLastStep
       })
     })
     .then(response=>response.json())
